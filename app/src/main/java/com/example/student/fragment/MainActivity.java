b9cae4ffa0736c09d5048bce319dba5b011ed277
ricky801216 <a0923966051@gmail.com>
2016-08-12 16:02:11 +0800

    靜態Fragment

---
+++ app/src/main/java/com/example/student/fragment/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.student.fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1(View v)
+    {
+        TextView tv = (TextView)findViewById(R.id.textView);
+        tv.setText("ABC");
+    }
+}
